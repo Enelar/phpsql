e0e2c01c82bbef6a19248302c22016855f70a97f
Kirill Berezin <enelar@develop-project.ru>
2014-09-22 22:52:35 -0400

    pgsql support array insertion

--- pgsql.php
+++ pgsql.php
@@ -21,6 +21,10 @@ class pgsql extends \phpsql\connector_interface
 
   public function Query( $q, $p = [] )
   {
+    foreach ($p as &$param)
+      if (is_array($param))
+        $param = pgArrayFromPhp($param, null);
+
     $res = pg_query_params($this->db, $q, $p);
 
     if (is_string($res))
@@ -78,3 +82,33 @@ class pgsql extends \phpsql\connector_interface
 
 include_once('phpsql.php');
 \phpsql::RegisterSchemeHandler("pgsql", "\phpsql\connectors\pgsql");
+
+// http://www.youlikeprogramming.com/2013/01/interfacing-postgresqls-hstore-with-php/ 
+function pgArrayFromPhp($array, $data_type = 'character varying')
+{
+  $array = (array) $array; // Type cast to array.
+  $result = [];
+  
+  foreach ($array as $entry)
+  { // Iterate through array.
+    if (is_array($entry)) // Supports nested arrays.
+    {
+      $result[] = pgArrayFromPhp($entry, $data_type);
+      continue;
+    }
+    
+    $entry = str_replace('"', '\\"', $entry); // Escape double-quotes.
+    $entry = pg_escape_string($entry); // Escape everything else.
+
+    if (is_numeric($entry))
+      $result[] = $entry;
+    else
+      $result[] = "'" . $entry . "'";
+  }
+ 
+  $ret = '{' . implode(',', $result) . '}';
+
+  if ($data_type !== null)
+    $ret .= '::' . $data_type . '[]'; // format
+  return $ret;
+}
