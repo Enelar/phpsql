192b953e5b8cb99a02391fb8dae2342ba3d144ee
Kirill Berezin <enelar@exsul.net>
2015-08-20 22:30:08 +0300

    Automatically convert json values if it possible

--- pgsql.php
+++ pgsql.php
@@ -50,22 +50,22 @@ class pgsql extends \phpsql\connector_interface
   {
     $this->Query("SAVEPOINT nested_transaction_{$name};");
   }
-  
+
   public function StepBack( $name )
   {
     $this->Query("ROLLBACK TO SAVEPOINT nested_transaction_{$name};");
   }
-  
+
   public function ForgetStep( $name )
   {
     $this->Query("RELEASE SAVEPOINT nested_transaction_{$name};");
   }
-  
+
   public function Rollback()
   {
     $this->Query("ROLLBACK;");
   }
-  
+
   public function Commit()
   {
     $this->Query("COMMIT;");
@@ -76,7 +76,7 @@ class pgsql extends \phpsql\connector_interface
     $stat = pg_transaction_status($this->db);
     return $stat === PGSQL_TRANSACTION_ACTIVE || $stat === PGSQL_TRANSACTION_INTRANS || $stat === PGSQL_TRANSACTION_INERROR;
   }
-  
+
   public function RawConnection()
   {
     return $this->db;
@@ -94,7 +94,13 @@ function array_recursive_extract($obj)
     if (is_array($row))
       $ret[$key] = array_recursive_extract($row);
     else if (isset($row[0]) && $row[0] == '{') // expect postgresql array
-      $ret[$key] = array_pg2php($row);
+    {
+      $json = json_decode($row, true);
+      if (!is_null($json))
+        $ret[$key] = $json;
+      else
+        $ret[$key] = array_pg2php($row);
+    }
     else
       $ret[$key] = $row;
   }
@@ -108,21 +114,21 @@ function array_pg2php($text)
     return [];
   if(!is_string($text) && $text == '{}')
     return [];
- 
+
   $text = substr($text, 1, -1); // Removes starting "{" and ending "}"
   if(substr($text, 0, 1) == '"')
     $text = substr($text, 1);
- 
+
   if(substr($text, -1, 1) == '"')
     $text = substr($text, 0, -1);
- 
-  $values = explode(',', $text);    
- 
+
+  $values = explode(',', $text);
+
   $fixed_values = [];
- 
+
   foreach ($values as $value)
     $fixed_values[] = str_replace(["'", "\""], '', $value);
- 
+
   return $fixed_values;
 }
 
@@ -131,7 +137,7 @@ function array_php2pg($array, $data_type = 'character varying')
 {
   $array = (array) $array; // Type cast to array.
   $result = [];
-  
+
   foreach ($array as $entry)
   { // Iterate through array.
     if (is_array($entry)) // Supports nested arrays.
@@ -139,7 +145,7 @@ function array_php2pg($array, $data_type = 'character varying')
       $result[] = array_php2pg($entry, $data_type);
       continue;
     }
-    
+
     $entry = str_replace('"', '\\"', $entry); // Escape double-quotes.
     $entry = pg_escape_string($entry); // Escape everything else.
 
@@ -148,7 +154,7 @@ function array_php2pg($array, $data_type = 'character varying')
     else
       $result[] = "'" . $entry . "'";
   }
- 
+
   $ret = '{' . implode(',', $result) . '}';
 
   if ($data_type !== null)
